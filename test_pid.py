849dc4b0461645aa4d4f57d3d568afc66ea17f39
rzeien <rzeien@dyn.com>
2017-01-23 16:10:46 -0500

    simple PID test script

---
+++ test_pid.py
@@ -0,0 +1,29 @@
+import random
+import time
+
+from PID import PID
+
+
+SET_POINT = 225
+INTERVAL = 0.25
+
+START_INPUT = 150
+INPUT = START_INPUT
+NUM_INPUTS = 100
+
+
+if __name__ == '__main__':
+    pid = PID(
+        set_point=SET_POINT,
+        interval=INTERVAL
+    )
+
+    for i in xrange(1, NUM_INPUTS):
+        # v = pid.calculate(random.randint(90, 110))
+        v = pid.calculate(INPUT)
+        print v
+
+        if INPUT != SET_POINT:
+            INPUT += 1
+
+        time.sleep(INTERVAL)
